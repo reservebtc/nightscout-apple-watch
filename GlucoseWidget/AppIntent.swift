6dbc21e896ec94aca8d738af67613b3db108e978
reserveBTC <reservebtcproof@gmail.com>
2025-10-27 12:22:24 +0700

    Update AppIntent.swift

--- GlucoseWidget/AppIntent.swift
+++ GlucoseWidget/AppIntent.swift
@@ -1,1 +1,19 @@
+//
+//  AppIntent.swift
+//  GlucoseWidget
+//
+//  Created on 20/10/25.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
 
