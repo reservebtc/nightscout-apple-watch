6e863d39b7fefb6932881a3e695b10083ef50d54
reserveBTC <reservebtcproof@gmail.com>
2025-10-27 12:22:54 +0700

    Update GlucoseWidgetBundle.swift

--- GlucoseWidget/GlucoseWidgetBundle.swift
+++ GlucoseWidget/GlucoseWidgetBundle.swift
@@ -1,1 +1,18 @@
+//
+//  GlucoseWidgetBundle.swift
+//  GlucoseWidget
+//
+//  Created on 20/10/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct GlucoseWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        GlucoseWidget()
+        GlucoseWidgetControl()
+    }
+}
 
